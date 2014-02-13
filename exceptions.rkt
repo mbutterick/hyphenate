49ced828e395626106388be43c1fbecd636d999b
Matthew Butterick <mb@mbtype.com>
2014-02-13 11:18:33 -0800

    split hyphenation-patterns into patterns file and exceptions file

---
+++ exceptions.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(provide default-exceptions)
+
+; Knuth and Liang's original exception patterns from classic TeX.
+; In the public domain.
+(define default-exceptions (map symbol->string '(as-so-ciate as-so-ciates dec-li-na-tion oblig-a-tory phil-an-thropic present presents project projects reci-procity re-cog-ni-zance ref-or-ma-tion ret-ri-bu-tion ta-ble)))
--- patterns.rkt
+++ patterns.rkt
@@ -1,10 +1,6 @@
 #lang racket/base
 
-(provide default-exceptions default-patterns)
-
-; Knuth and Liang's original exception patterns from classic TeX.
-; In the public domain.
-(define default-exceptions (map symbol->string '(as-so-ciate as-so-ciates dec-li-na-tion oblig-a-tory phil-an-thropic present presents project projects reci-procity re-cog-ni-zance ref-or-ma-tion ret-ri-bu-tion ta-ble)))
+(provide default-patterns)
 
 (define default-patterns 
   ; Knuth and Liang's original hyphenation patterns from classic TeX.
