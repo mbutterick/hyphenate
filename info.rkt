d2e915bf4899f7ed8f10a4c3ed56476818670185
Matthew Butterick <mb@mbtype.com>
2014-10-19 10:30:30 -0700

    update dependencies

--- info.rkt
+++ info.rkt
@@ -2,3 +2,5 @@
 (define collection "hyphenate")
 (define scribblings '(("scribblings/hyphenate.scrbl" ())))
 (define deps '("txexpr"))
+(define build-deps '("scribble-lib"))
+(define compile-omit-paths '("tests.rkt"))
