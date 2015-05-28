a1c3cc506e03d96cf5015cfbdc63e557f4efedc9
Matthew Butterick <mb@mbtype.com>
2015-05-28 11:43:04 -0700

    add update-implies

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection 'multi)
 (define deps '("base" "sugar" "txexpr" "typed-racket-lib" "typed-racket-more" "rackunit-lib"))
+(define update-implies '("txexpr" "sugar"))
 (define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc"))
-(define test-omit-paths '("typed/hyphenate/patterns-hashed.rkt"))
