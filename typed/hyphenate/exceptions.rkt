43963adc3cda37de67982a4df392cd02e1919d38
Matthew Butterick <mb@mbtype.com>
2015-08-18 13:11:12 -0700

    add a few exceptions

--- typed/hyphenate/exceptions.rkt
+++ typed/hyphenate/exceptions.rkt
@@ -4,5 +4,11 @@
 
 ; Knuth and Liang's original exception patterns from classic TeX.
 ; In the public domain.
-(define: default-exceptions : Patterns
+(define: kl-exceptions : Patterns
   (map symbol->string '(as-so-ciate as-so-ciates dec-li-na-tion oblig-a-tory phil-an-thropic present presents project projects reci-procity re-cog-ni-zance ref-or-ma-tion ret-ri-bu-tion ta-ble)))
+
+(define: mb-exceptions : Patterns
+  (map symbol->string '(real-ly law-yer law-yers law-yered law-yer-ing law-yer-ly)))
+
+(define: default-exceptions : Patterns
+  (append kl-exceptions mb-exceptions))
