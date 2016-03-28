ef91a2465a261469c714db9e8f4fc6dab7bd3dcd
Matthew Butterick <mb@mbtype.com>
2016-03-27 17:23:50 -0700

    adjust info.rkt

--- info.rkt
+++ info.rkt
@@ -4,5 +4,3 @@
 (define deps '("base" ["sugar" #:version "0.2"] ["txexpr" #:version "0.2"]))
 (define update-implies '("txexpr" "sugar"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("hyphenate/scribblings/hyphenate.scrbl" ())))
-(define compile-omit-paths '("hyphenate/tests.rkt"))
